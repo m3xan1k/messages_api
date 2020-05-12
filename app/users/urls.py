611132f3d63c7c9ec0eb8f97e4d71c49630501dd
Shevtsov Sergey <dev.serzh@gmail.com>
2020-05-12 18:33:17 +0300

    login

--- app/users/urls.py
+++ app/users/urls.py
@@ -4,5 +4,6 @@ from users import views
 
 urlpatterns = [
     path('users/', views.UsersView.as_view(), name=views.UsersView.name),
+    path('users/log/', views.UserLogView.as_view(), name=views.UserLogView.name),
     path('users/<str:id>/', views.UserView.as_view(), name=views.UserView.name),
 ]
--- app/users/views.py
+++ app/users/views.py
@@ -3,8 +3,10 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import authenticate, login, logout
 
 
 class UsersView(APIView):
@@ -35,6 +37,9 @@ class UserView(APIView):
     name = 'user'
 
     def get(self, request: Request, id: str) -> Response:
+        """
+        Returns single user instance by id
+        """
         if not id.isnumeric():
             return Response(data={'err': 'id should be numeric'},
                             status=status.HTTP_422_UNPROCESSABLE_ENTITY)
@@ -43,9 +48,23 @@ class UserView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class LoginView(APIView):
+class UserLogView(APIView):
 
-    name = 'login'
+    name = 'user_log'
+
+    def get(self, request: Request) -> Response:
+        logout(request)
+        return Response({'index_url': reverse('index')}, request=request)
 
     def post(self, request: Request) -> Response:
-        pass
+        """
+        Log In user
+        """
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response_data = {'index_url': reverse('index'), 'msg': 'Successfull login'}
+            return Response(data=response_data, status=status.HTTP_200_OK)
+        return Response({'err': 'Invalid login'}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
